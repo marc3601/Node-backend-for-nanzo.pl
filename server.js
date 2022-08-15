6915557ffdd6466b4b2fa1b62d31add675557991
Marcin Czaja <marcin.czaja2@gmail.com>
2022-08-15 12:45:53 +0200

    domain change
    Former-commit-id: e9ea2876cd85b922197bda99ff80b4d1a1f651fd

--- server.js
+++ server.js
@@ -363,7 +363,7 @@ app.post(
                 await uploadFile(`upload/result${id}.jpeg`, item).catch((err) =>
                   console.log(err)
                 );
-                const url = `http://localhost:8080/images/${item.filename}`;
+                const url = `https://webdev-online.pl/images/${item.filename}`;
                 image.push({
                   width: data.width,
                   height: data.height,
@@ -418,7 +418,7 @@ app.post(
                   .then(async () => {
                     await probe(fs.createReadStream(path))
                       .then((data) => {
-                        const url = `http://localhost:8080/images/${name}`;
+                        const url = `https://webdev-online.pl/images/${name}`;
                         gif.width = data.width;
                         gif.height = data.height;
                         gif.url = url;
