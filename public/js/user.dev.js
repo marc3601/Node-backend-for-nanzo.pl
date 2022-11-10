bcab358ad9c0b65fa650a3536af1be26ba87df52
Marcin Czaja <marcin.czaja2@gmail.com>
2022-11-10 20:06:16 +0100

    Users view bug fixes & improvements

--- public/js/user.dev.js
+++ public/js/user.dev.js
@@ -1,12 +1,11 @@
 const btns = document.querySelectorAll(".btn_expand");
-// console.log(btns[0].parentNode.id);
+const btn_b = document.querySelector(".back_btn");
 
+const body = document.querySelector("body");
 btns.forEach((item) => {
-  //   console.log(item.previousElementSibling.classList);
   item.addEventListener("click", () => {
     const margin = `${item.previousElementSibling.offsetHeight + 10}px`;
     const extend = `-${item.previousElementSibling.offsetHeight}px`;
-    console.log(margin);
     if (item.parentNode.style.marginBottom !== margin) {
       item.parentNode.style.marginBottom = margin;
       item.previousElementSibling.style.marginBottom = extend;
@@ -18,3 +17,32 @@ btns.forEach((item) => {
     }
   });
 });
+
+function throttle(func, wait) {
+  let waiting = false;
+  return function () {
+    if (waiting) {
+      return;
+    }
+
+    waiting = true;
+    setTimeout(() => {
+      func.apply(this, arguments);
+      waiting = false;
+    }, wait);
+  };
+}
+
+const onScroll = throttle(() => {
+  if (Math.round((window.scrollY / body.offsetHeight) * 100) > 50) {
+    if (btn_b.style.right !== "50px") {
+      btn_b.style.right = "50px";
+    }
+  } else {
+    if (btn_b.style.right !== "-50px") {
+      btn_b.style.right = "-50px";
+    }
+  }
+}, 100);
+
+document.addEventListener("scroll", onScroll);
