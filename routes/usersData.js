23ec0666372081d40ed2422f5ed601c575dfd9aa
Marcin Czaja <marcin.czaja2@gmail.com>
2023-06-05 22:34:11 +0200

    Pagination change

--- routes/usersData.js
+++ routes/usersData.js
@@ -2,23 +2,27 @@ const User = require("../database/schemas/userSchema");
 const usersData = async (req, res) => {
   if (req.query.page) {
     let page;
-    try {
-      page = parseInt(req.query.page);
-    } catch (error) {
-      console.error(error);
+    page = parseInt(req.query.page);
+    if (page % 1 !== 0) {
+      page = Math.round(page);
     }
+    const offset = page * 15;
     User.paginate(
       {},
-      { offset: page, limit: 50, sort: { _id: -1 } },
+      { offset: offset ? offset : 0, limit: 15, sort: { _id: -1 } },
       (err, data) => {
         if (err) return console.error(err);
-        res.render("users", { data: data });
+        if (data.total - data.offset > 0) {
+          res.render("users", { data: data });
+        } else {
+          res.render("users", { data: { ...data, offset: 0 } });
+        }
       }
     );
   } else {
     User.paginate(
       {},
-      { offset: 0, limit: 50, sort: { _id: -1 } },
+      { offset: 0, limit: 15, sort: { _id: -1 } },
       (err, data) => {
         if (err) return console.error(err);
         res.render("users", { data: data });
