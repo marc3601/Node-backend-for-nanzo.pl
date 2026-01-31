800e99bdd26796edfe934855897078fc67c66085
Marcin <marcin.czaja2@gmail.com>
2026-01-31 13:57:15 +0100

    Update login page: modern design, ripple effect, logo integration

--- public/js/login.dev.js
+++ public/js/login.dev.js
@@ -52,6 +52,28 @@ const warning = `<span><?xml version="1.0" encoding="iso-8859-1"?>
 </g>
 </svg></span>`;
 
+// Ripple effect function
+button.addEventListener("click", function(e) {
+  const ripple = document.createElement("span");
+  ripple.classList.add("ripple");
+  
+  const rect = this.getBoundingClientRect();
+  const size = Math.max(rect.width, rect.height);
+  const x = e.clientX - rect.left - size / 2;
+  const y = e.clientY - rect.top - size / 2;
+  
+  ripple.style.width = ripple.style.height = size + "px";
+  ripple.style.left = x + "px";
+  ripple.style.top = y + "px";
+  
+  this.appendChild(ripple);
+  
+  setTimeout(() => {
+    ripple.remove();
+  }, 600);
+});
+
+// Login functionality
 button.addEventListener("click", (e) => {
   e.preventDefault();
   data.username = username.value;
